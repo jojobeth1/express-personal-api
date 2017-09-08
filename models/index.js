f148d15d6ba8f13ae6542c637bc51e9aacd6bb17
jojobeth1@gmail.com <jojobeth1@gmail.com>
2017-09-07 17:29:03 -0700

    another

--- models/index.js
+++ models/index.js
@@ -1,4 +1,9 @@
 var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
 
+//var promise = mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/personal-api", {
+//  useMongoClient: true,
+//});
+
 // module.exports.Campsite = require("./campsite.js.example");
+module.exports.Projects = require("./projects.js");
